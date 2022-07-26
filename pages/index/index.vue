db055ffcc433a93742dccbe9479429e16fdcd797
wangzw <wzw71603974@qq.com>
2022-07-26 17:48:37 +0800

    init project

---
+++ pages/index/index.vue
@@ -0,0 +1,58 @@
+<template>
+	<view class="content">
+		
+		<u--text type="primary" text="主色"></u--text>
+		<u--text type="error"   text="错误"></u--text>
+		<u--text type="success" text="成功"></u--text>
+		<u--text type="warning" text="警告"></u--text>
+		<u--text type="info"    text="信息"></u--text>
+		<!-- <image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view> -->
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
