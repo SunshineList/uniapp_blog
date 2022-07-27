a2c39937735109b5e1169dff190cb091c180b646
wangzw <wzw571603974@qq.com>
2022-07-27 23:06:44 +0800

    feat: add scroll list

--- components/navigationBar/navigationBar.vue
+++ components/navigationBar/navigationBar.vue
@@ -1,12 +1,34 @@
 <template>
-	<view class="u-page">
+	<view>
 		<u-scroll-list :indicator="indicator">
-			<u-col span="3" v-for="(item, index) in articleList" :key="index">
-				<view class="demo-layout bg-purple">
-					<text>{{item.name}}</text>
-				</view>
-			</u-col>
+			<u-row justify="space-between">
+				<u-col span="3" v-for="(item, index) in articleList" :key="index">
+					<view class="demo-layout bg-purple" @click="clickCategory(item.id, index)">
+						<text :class="nowIndex == index ? 'click_class' : '' ">{{item.name}}</text>
+					</view>
+				</u-col>
+			</u-row>
 		</u-scroll-list>
+		
+		<view class="uni-list">
+			<view class="uni-list-cell" hover-class="uni-list-cell-hover" v-for="(item,index) in list" :key="index">
+				<view class="uni-media-list">
+					<image class="uni-media-list-logo" :src="item.author_avatar"></image>
+					<view class="uni-media-list-body">
+						<view class="uni-media-list-text-top">{{item.title}}</view>
+						<view class="uni-media-list-text-bottom uni-ellipsis">{{item.summary}}</view>
+					</view>
+				</view>
+			</view>
+		</view>
+		<!-- <uni-table stripe emptyText="暂无更多数据">
+			<uni-tr>
+				<uni-th align="center"></uni-th>
+			</uni-tr>
+			<uni-tr>
+				<uni-td></uni-td>
+			</uni-tr>
+		</uni-table> -->
 	</view>
 </template>
 
@@ -16,6 +38,9 @@
 		data() {
 			return {
 				indicator: false,
+				nowIndex: 0,
+				list: [],
+				textClass: "",
 				articleList: [{
 						"id": 1,
 						"name": "军事"
@@ -40,11 +65,39 @@
 						"id": 6,
 						"name": "汽车"
 					},
+					{
+						"id": 7,
+						"name": "汽车"
+					},
 				]
 			};
 		},
+		
+		created() {
+			uni.showLoading({
+				title: "加载中...."
+			})
+			uni.request({
+				url: 'https://unidemo.dcloud.net.cn/api/news',
+				method: 'GET',
+				data: {},
+				success: res => {
+					this.list = res.data
+					uni.hideLoading()
+				},
+				fail: () => {},
+				complete: () => {}
+			});
+		
+		},
+		
 		methods: {
-			
+			clickCategory(categoryId, index) {
+				if (this.nowIndex == index) {
+					return // 如果点击的时候发现id和本次id一致就不请求数据
+				}
+				this.nowIndex = index
+			}
 		}
 	}
 </script>
@@ -54,6 +107,12 @@
 		padding: 12px;
 	}
 
+	.click_class {
+		// font-size: 48rpx;
+		font-weight: bolder;
+		color: red;
+	}
+
 	.demo-layout {
 		height: 25px;
 		border-radius: 4px;
@@ -61,8 +120,11 @@
 
 	.bg-purple {
 		// background: #;
-		background: #f6f5f8;
+		background: lightcyan;
+		// background: red;
 		text-align: center;
+		// padding-left: 20rpx;
+		margin-left: 8rpx;
 	}
 
 	.bg-purple-light {
@@ -73,13 +135,9 @@
 		background: #99a9bf;
 	}
 	
-	.u-scroll-list{
-		// u-scroll-list__scroll-view__content
-		&__scroll-view{
-			&__content{
-				display: flex;
-				justify-content: space-between;
-			}
-		}
+	.uni-media-list-body{
+		height: auto;
+		line-height: 1.6em;
 	}
+	
 </style>
